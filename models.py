02df63befd25d84b2321e4fd7bea5fe8556e14a9
projeta618 <projeta618@gmail.com>
2024-02-14 22:11:05 -0300

    hore: Add migration for notification message object and translation
    The migrations files `0004_notificationmessage_object_and_more.py` and `0005_rename_jobtranslate_notificationmessagetranslate.py` were added to implement changes related to the notification message object and its translation.
    
    Changes:
    - Added a new migration file `0004_notificationmessage_object_and_more.py` to add a new field `object` to the `NotificationMessage` model.
    - Modified the `NotificationMessage` model to have a

--- models.py
+++ models.py
@@ -5,6 +5,9 @@ This module contains the Notification model.
 from django.db import models
 from kernel.models.base_metadata_model import BaseMetadataModel
 from notification.rules.stack import NOTIFICATION_RULESTACK
+from django.forms.models import model_to_dict
+from kernel.models.serialize import serializer__serialize__, serializer__init__
+from kernel.i18n.models import translateDBQuerySet, translateDBObject
 
 class Notification(BaseMetadataModel):
     """
@@ -47,6 +50,51 @@ class Notification(BaseMetadataModel):
         """
         return ''
     
+
+class NotificationMessageTranslate(BaseMetadataModel):
+    """
+        @description:
+    """
+    language = models.CharField(
+        'language',
+        max_length=255,
+        default='fr',
+        choices=(
+            ('fr', 'fr'),
+        ),
+    )
+    name = models.CharField(
+        'name',
+        max_length=255,
+        blank=True,
+        null=True,
+    )
+
+    description = models.TextField(
+        'description',
+        blank=True,
+        null=True,
+    )
+
+    translateObject = models.ForeignKey(
+        'notification.NotificationMessage',
+        on_delete=models.CASCADE,
+        related_name='translates',
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self) -> str:
+        return self.language
+    
+    @serializer__serialize__
+    def serialize(self, request):
+        """
+            @description: 
+        """
+        serialize = model_to_dict(self)
+        return serialize
+
 class NotificationMessage(BaseMetadataModel):
     """
     This class represents a notification message.
@@ -59,8 +107,28 @@ class NotificationMessage(BaseMetadataModel):
         choices=NOTIFICATION_RULESTACK.models_choices()
     )
 
+    object = models.CharField(
+        max_length=255, 
+        null=True, 
+        blank=True,
+    )
+
     message = models.TextField(
         null=True, 
         blank=True,
         unique=True,
     )
+
+    @serializer__serialize__
+    def serialize(self, request):
+        """
+        Returns the serialized object.
+        """
+        serialize = model_to_dict(self)
+        return serialize
+    
+    def __str__(self):
+        """
+        Returns the string representation of the notification message.
+        """
+        return self.message
