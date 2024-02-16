fa870acc9ca3365b9c0dea924f6cb57db6f72340
projeta618 <projeta618@gmail.com>
2024-02-15 21:12:57 -0300

    chore: Update apps.py and tests.py
    Update the configuration in apps.py and add a new rule in tests.py for notifications.
    
    Changes:
    - Modified the configuration in apps.py to include a description and a class representation of the notification app.
    - Added a new rule class called WelcomeNotification in tests.py, along with its methods for checking, running, handling errors, responding, clicking, and opening notifications.
    - Added the WelcomeNotification rule to the notification rule stack.
    - Updated the NotificationTestCase in

--- apps.py
+++ apps.py
@@ -1,6 +1,21 @@
+"""
+This file represents the configuration of the notification app.
+"""
+
 from django.apps import AppConfig
 
 
 class NotificationConfig(AppConfig):
+    """
+    This class represents the configuration of the notification app.
+    """
+
+    """
+    The default auto field.
+    """
     default_auto_field = "django.db.models.BigAutoField"
+
+    """
+    The name of the app.
+    """
     name = "notification"
--- tests.py
+++ tests.py
@@ -1,3 +1,59 @@
 from django.test import TestCase
 
-# Create your tests here.
+from notification.rules.default_notification import DefaultRuleClass
+from notification.rules.stack import NOTIFICATION_RULESTACK
+
+class WelcomeNotification(DefaultRuleClass):
+    """
+        @description: This class is the avatar file rule
+    """
+    """
+    The label of the rule.
+    """
+    label = 'WELCOME'
+
+    """
+    Whether the rule is allowed.
+    """
+    allow = True
+
+    def __init__(self):
+        super(WelcomeNotification, self).__init__()
+    
+    def check(self, *args, **kwargs):
+        return True
+    
+    def run(self, *args, **kwargs):
+        return True
+
+    def error(self, *args, **kwargs):
+        return True
+    
+    def response(self, *args, **kwargs):
+        return True
+    
+    def click(self, *args, **kwargs):
+        return True
+
+    def open(self, *args, **kwargs):
+        return True
+
+NOTIFICATION_RULESTACK.set_rule(WelcomeNotification())
+
+
+class NotificationTestCase(TestCase):
+    """
+    This class represents the notification test case.
+    """
+
+    def setUp(self):
+        """
+        Set up the test case.
+        """
+        pass
+
+    def test_notification(self):
+        """
+        Test the notification.
+        """
+        pass
