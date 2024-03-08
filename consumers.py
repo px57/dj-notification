3ba4b8b78aa7b75b4d1c30c9be5a2b5dff91970c
px57 <david@prepera.io>
2024-03-07 22:52:33 -0300

    feat: Add NotificationConsumer class to consumers.py
    This change involves adding a new file `consumers.py` and defining a `NotificationConsumer` class inside it. The class includes methods like `connect`, `receive`, `receive__initial_settings`, `receive__new_message`, `receive__init`, `receive__create_personnal_room`, and `receive__delete_room` with corresponding descriptions.

---
+++ consumers.py
@@ -0,0 +1,49 @@
+
+import json
+
+from channels.generic.websocket import WebsocketConsumer
+from chatroom.managers.chat_room import CHATROOM_MANAGER
+from kernel.websocket.decorators import websocket__simplifier
+from kernel.http.request import FakeRequest
+from kernel.interfaces.decorators import load_interface
+
+class NotificationConsumer(WebsocketConsumer):
+    """
+
+    """
+
+    def connect(self):
+        """
+        """
+
+    def receive(self, text_data):
+        """
+        """
+
+    def receive__initial_settings(self, settings):
+        """
+    
+        """
+        pass
+
+    def receive__new_message(self, data):
+        """
+            @description:
+        """
+
+
+    def receive__init(self, message):
+        """
+            @description: Init.
+        """
+
+    def receive__create_personnal_room(self, message):
+        """
+            @description: Create a personnal room.
+        """
+
+
+    def receive__delete_room(self, chatroom):
+        """
+            @description: 
+        """
